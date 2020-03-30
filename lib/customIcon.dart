c2bddd37972d89295c41dbcaf9f39abc1f1328c0
ishihara <r.funk.7188@gmail.com>
2020-03-30 09:09:22 +0900

    ヘッダーアイコンを追加

---
+++ lib/customIcon.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CustomIcons {
+  static const IconData menu = IconData(0xe5d2, fontFamily: 'MaterialIcons');
+  static const IconData option = IconData(0xe902, fontFamily: 'CustomIcons');
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uistoryapp/customIcon.dart';
 
 void main() => runApp(
       MaterialApp(
@@ -17,7 +18,41 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF2d3447),
-      body: SingleChildScrollView(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 12,
+                right: 12,
+                top: 30,
+                bottom: 8,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(
+                      CustomIcons.menu,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
