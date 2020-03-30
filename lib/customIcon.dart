805590de66c6e203cfd209bab01afe6d41d67b91
ishihara <r.funk.7188@gmail.com>
2020-03-30 21:59:30 +0900

    スクロールアニメーション完成

--- lib/customIcon.dart
+++ lib/customIcon.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 class CustomIcons {
   static const IconData menu = IconData(0xe5d2, fontFamily: 'MaterialIcons');
-  static const IconData option = IconData(0xe902, fontFamily: 'CustomIcons');
+  static const IconData option = IconData(58819, fontFamily: 'MaterialIcons');
 }
---
+++ lib/data.dart
@@ -0,0 +1,13 @@
+List<String> images = [
+  "assets/image_04.png",
+  "assets/image_03.png",
+  "assets/image_02.png",
+  "assets/image_01.png",
+];
+
+List<String> title = [
+  "Hounted Ground",
+  "Fallen In Love",
+  "The Dreaming Moon",
+  "Jack the Persian and the Black Castel",
+];
